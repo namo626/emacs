9072dba4825e386cab3ee6df8c404914051e0a29
namo626 <namo26june@gmail.com>
2018-06-05 11:20:05 -0400

    anaconda python

--- init.el
+++ init.el
@@ -21,18 +21,11 @@
  '(company-quickhelp-color-background "#4F4F4F")
  '(company-quickhelp-color-foreground "#DCDCCC")
  '(compilation-message-face (quote default))
- ;;'(cua-global-mark-cursor-color "#2aa198")
- ;;'(cua-normal-cursor-color "#839496")
- ;;'(cua-overwrite-cursor-color "#b58900")
- ;;'(cua-read-only-cursor-color "#859900")
  '(custom-enabled-themes (quote (zenburn)))
  '(custom-safe-themes
    (quote
     ("a914fce8ef585e826ea136da5478bc929a3a42161cd478e4f4ff6df00d8da1c3" "c3e6b52caa77cb09c049d3c973798bc64b5c43cc437d449eacf35b3e776bf85c" "e11569fd7e31321a33358ee4b232c2d3cf05caccd90f896e1df6cab228191109" "5e52ce58f51827619d27131be3e3936593c9c7f9f9f9d6b33227be6331bf9881" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "7f3ef7724515515443f961ef87fee655750512473b1f5bf890e2dc7e065f240c" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "125fd2180e880802ae98b85f282b17f0aa8fa6cb9fc4f33d7fb19a38c40acef0" "dc9a8d70c4f94a28aafc7833f8d05667601968e6c9bf998791c39fcb3e4679c9" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "bcc6775934c9adf5f3bd1f428326ce0dcd34d743a92df48c128e6438b815b44f" "cdfc5c44f19211cfff5994221078d7d5549eeb9feda4f595a2fd8ca40467776c" default)))
- ;;'(evil-emacs-state-cursor (quote ("#D50000" hbar)) t)
- ;;'(evil-insert-state-cursor (quote ("#D50000" bar)) t)
- ;;'(evil-normal-state-cursor (quote ("#F57F17" box)) t)
- ;;'(evil-visual-state-cursor (quote ("#66BB6A" box)) t)
+ '(elpy-rpc-python-command "/var/namo/anaconda2/bin/python")
  '(fci-rule-color "#073642")
  '(flycheck-color-mode-line-face-to-color (quote mode-line-buffer-id))
  '(frame-background-mode (quote dark))
@@ -85,11 +78,12 @@
  '(org-tags-column 90)
  '(package-selected-packages
    (quote
-    (sunburn-theme apropospriate-theme auto-complete matlab-mode elpy sicp racket-mode auctex slime intero magit company-ghc hindent ghc haskell-mode pdf-tools color-theme-sanityinc-tomorrow gruvbox-theme color-theme-sanityinc-solarized hc-zenburn-theme zenburn-theme linum-relative org-edna)))
+    (ein company-auctex auto-complete-auctex sunburn-theme apropospriate-theme auto-complete matlab-mode elpy sicp racket-mode auctex slime intero magit company-ghc hindent ghc haskell-mode pdf-tools color-theme-sanityinc-tomorrow gruvbox-theme color-theme-sanityinc-solarized hc-zenburn-theme zenburn-theme linum-relative org-edna)))
  '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838")))
  '(pos-tip-background-color "#073642")
  '(pos-tip-foreground-color "#93a1a1")
- '(python-shell-interpreter "python2")
+ '(python-shell-interpreter "/var/namo/anaconda2/bin/ipython")
+ '(python-shell-interpreter-args "--simple-prompt")
  '(server-mode t)
  '(show-paren-mode t)
  '(smartrep-mode-line-active-bg (solarized-color-blend "#859900" "#839496" 0.2))
@@ -143,6 +137,7 @@
 (evil-set-initial-state 'haskell-interactive-mode 'emacs)
 (evil-set-initial-state 'slime-mode 'emacs)
 (evil-set-initial-state 'racket-repl-mode 'emacs)
+(evil-set-initial-state 'ein:notebooklist-mode 'emacs)
 ;;(define-key evil-normal-state-map "\C-r" 'undo-tree-redo)
 
 
@@ -166,14 +161,14 @@
   (add-to-list 'exec-path my-stack-path))
 (autoload 'ghc-init "ghc" nil t)
 (autoload 'ghc-debug "ghc" nil t)
-(add-hook 'haskell-mode-hook (lambda () (ghc-init)))
+;;(add-hook 'haskell-mode-hook (lambda () (ghc-init)))
 (setq haskell-process-type 'stack-ghci)
 (setq haskell-process-path-ghci "stack")
 ;;(add-hook 'haskell-mode-hook #'hindent-mode)
 
 ;;autocompletion
 (require 'company)
-(add-hook 'haskell-mode-hook 'company-mode)
+;;(add-hook 'haskell-mode-hook 'company-mode)
 (add-to-list 'company-backends 'company-ghc)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
 (add-hook 'c-mode-common-hook 'company-mode)
@@ -323,6 +318,9 @@
           (lambda ()
             (auto-complete-mode 1)))
 
+;; python
+(elpy-enable)
+
 ;;prettifier
 ;;(defun my-add-pretty-lambda ()
 ;;  "convert stuff to unicode"
@@ -333,4 +331,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#3F3F3F" :foreground "#DCDCCC" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 145 :width normal :foundry "PfEd" :family "Inconsolata")))))
+ '(default ((t (:inherit nil :stipple nil :background "#3F3F3F" :foreground "#DCDCCC" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "CYRE" :family "Inconsolata")))))
