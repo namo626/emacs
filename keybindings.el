f6af79e68370871374a7197d1f704b5131dce5fb
namo626 <namo26june@gmail.com>
2019-04-17 12:00:54 -0400

    Modify keybindings

--- keybindings.el
+++ keybindings.el
@@ -10,8 +10,19 @@
   (interactive)
   (shrink-window (/ (window-height (next-window)) 2)))
 
+;; https://gist.github.com/mads-hartmann/3402786
+(defun toggle-maximize-buffer ()
+  "Maximize buffer"
+  (interactive)
+  (if (= 1 (length (window-list)))
+      (jump-to-register '_)
+    (progn
+      (window-configuration-to-register '_)
+      (delete-other-windows))))
+
 (global-set-key (kbd "<f7>") 'halve-other-window-height)
 (global-set-key (kbd "<f6>") 'halve-current-window-height)
+(global-set-key (kbd "M-y") 'toggle-maximize-buffer)
 
 
 ;; general usage
@@ -25,5 +36,7 @@
 (global-set-key (kbd "M-1") (kbd "C-x 1"))
 (global-set-key (kbd "M-2") (kbd "C-x 2"))
 (global-set-key (kbd "M-e") 'dante-eval-block)
+(global-set-key (kbd "M-c") 'comment-region)
+(global-set-key (kbd "M-u") 'uncomment-region)
 (global-set-key [f4] 'winner-undo)
 (global-set-key [f5] 'winner-redo)
